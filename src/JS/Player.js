4338f51ab03ffeb336b14e42e4864bf06069b539
Rainbow <zyx3219428988@163.com>
2026-01-06 20:08:45 +0800

    添加了基本游戏逻辑

--- src/JS/Player.js
+++ src/JS/Player.js
@@ -1,19 +1,8 @@
 ﻿export class Player {
-	constructor() {
-		this.button = document.getElementById("wordBtn");
+	conductor(){
 
-		// 使用可选链 (?.) 或 if 判断
-		if (this.button) {
-			this.button.addEventListener("click", () => {
-				this.onWordClick();
-			});
-		} else {
-			console.error("找不到按钮元素，请检查选择器或加载时机。");
-		}
-	}
+		
+	}	
 
-	onWordClick() {
-		console.log("Player：按钮被点击");
-	}
 }
 
